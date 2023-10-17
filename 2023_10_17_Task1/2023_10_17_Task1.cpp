cc32a118e5457ad643203813279e3cc0f4c18da2
Owner PC <PC-Owner-t@outlook.jp>
2023-10-17 16:19:38 +0900

    プロジェクト ファイルを追加します。

---
+++ 2023_10_17_Task1/2023_10_17_Task1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int time = 0;
+
+    printf("秒数を入力してください\n> ");
+    cin >> time;
+
+    printf("%d:%02d秒", time / 60, time % 60);
+}
